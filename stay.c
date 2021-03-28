b7a71303e7b318bba326d21e1f7a17022e420ba7
sme-ek <68247562+sme-ek@users.noreply.github.com>
2021-03-28 14:22:49 -0400

    add main game loop

--- stay.c
+++ stay.c
@@ -7,3 +7,19 @@
 #include <stdlib.h>
 
 GameScreen currentScreen;
+
+const int screenWidth = 1024;
+const int screenHeight = 768;
+static void UpdateScreen();
+
+int main(void){
+    InitWindow(screenWidth, screenHeight, "i wish you were here");
+    SetTargetFPS(60);
+//screen initiation here
+    while (!WindowShouldClose()){
+        UpdateScreen();
+    }
+}
+static void UpdateScreen(void){
+    //TODO write UpdateScreen function and define screen transitions
+}
